570db0b222366c293ea513174a21c4bc9a9529a3
cryptonaut <nrathman@ironcladtech.ca>
2015-05-13 21:45:10 -0700

    changed /api/v1/payment endpoint to plural version

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,9 +4,9 @@
 Route::get('/', 'WelcomeController@index');
 
 /* API methods */
-Route::get('api/v1/payment/request/{slotId}', 'API\PaymentController@request');
-Route::get('api/v1/payment/{paymentId}', 'API\PaymentController@get');
-Route::post('api/v1/payment/{paymentId}/cancel', 'API\PaymentController@cancel');
+Route::get('api/v1/payments/request/{slotId}', 'API\PaymentController@request');
+Route::get('api/v1/payments/{paymentId}', 'API\PaymentController@get');
+Route::post('api/v1/payments/{paymentId}/cancel', 'API\PaymentController@cancel');
 
 Route::get('api/v1/slots', 'API\SlotsController@all');
 Route::get('api/v1/slots/{slotId}', 'API\SlotsController@get');
