b404cf2387f2086cf8a4226e1f959b244ccede4a
cryptonaut <nrathman@ironcladtech.ca>
2015-07-04 18:08:54 -0700

    changed xchain settings to use environment variables

--- config/settings.php
+++ config/settings.php
@@ -1,9 +1,9 @@
 <?php
 
 return array(
-	'xchain_url' => '',
-	'xchain_user' => '',
-	'xchain_secret' =>'',
+	'xchain_url' => env('XCHAIN_CONNECTION_URL'),
+	'xchain_user' => env('XCHAIN_API_TOKEN'),
+	'xchain_secret' =>env('XCHAIN_API_KEY'),
 	'sweep_tx_fee' => 1000,
 	'sweep_tx_dust' => 5430,
 	'sweep_fuel_source' => '',
