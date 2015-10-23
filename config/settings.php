980e208be6e6ee817b6bfa16011e05b5e723e8ae
cryptonaut <nrathman@ironcladtech.ca>
2015-10-23 15:19:40 -0700

    added bitcrystals to supported peggable tokens

--- config/settings.php
+++ config/settings.php
@@ -9,7 +9,7 @@ return array(
 	'min_fuel_cost' => 5000,
 	'sweep_fuel_source' => env('TOKENSLOT_FUEL_SOURCE'),
 	'sweep_fuel_source_uuid' => env('TOKENSLOT_FUEL_UUID'),
-	'peggable_tokens' => array('BTC','LTBC','FLDC','GEMZ','SWARM','SJCX','XCP'),
+	'peggable_tokens' => array('BTC','LTBC','FLDC','GEMZ','SWARM','SJCX','XCP','BITCRYSTALS'),
 	'peg_currencies' => array('USD', 'BTC'),
 	'peg_currency_denoms' => array('USD' => 100, 'BTC' => 100000000),
 	'peggable_token_aliases' => array('LTBCOIN' => 'LTBC'),
