c094656d1461df00715dc5686b298be47aabd08c
cryptonaut <nrathman@ironcladtech.ca>
2016-02-07 17:26:54 -0800

    made payment requests work with POST method

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,6 +6,7 @@ Route::get('/', 'HomeController@index');
 /* API methods */
 Route::get('api/v1/payments/all', 'API\PaymentController@all');
 Route::get('api/v1/payments/request/{slotId}', 'API\PaymentController@request');
+Route::post('api/v1/payments/request/{slotId}', 'API\PaymentController@request');
 Route::get('api/v1/payments/{paymentId}', 'API\PaymentController@get');
 Route::post('api/v1/payments/{paymentId}/cancel', 'API\PaymentController@cancel');
 
