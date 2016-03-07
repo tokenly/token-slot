041f0940c3291c4e3bd9574bd67fffc49b5f5c61
cryptonaut <nrathman@ironcladtech.ca>
2016-03-07 14:40:47 -0800

    fixed syntax error..

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,6 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 
 abstract class Controller extends BaseController {
 
-	use DispatchesJob ValidatesRequests;
+	use DispatchesJob, ValidatesRequests;
 
 }
