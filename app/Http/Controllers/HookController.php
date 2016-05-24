12c3ebf4dbdb2ce4fe67c13ca0a3a09da97ed730
cryptonaut <nrathman@ironcladtech.ca>
2016-05-24 12:56:23 -0700

    fixed Slot class inclusion on webhook controller

--- app/Http/Controllers/HookController.php
+++ app/Http/Controllers/HookController.php
@@ -1,7 +1,7 @@
 <?php namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Tokenly\CurrencyLib\CurrencyUtil as Currency;
-use Config, Input, Slot, Payment;
+use Config, Input, App\Models\Slot, Payment;
 class HookController extends Controller {
 
 	/**
