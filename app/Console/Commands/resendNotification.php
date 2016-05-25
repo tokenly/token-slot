252cc0ea50a09f87679a7945e0104a37fca4cb65
cryptonaut <nrathman@ironcladtech.ca>
2016-05-25 16:09:38 -0700

    added option to resendNotification command to resend everything since a certain date

--- app/Console/Commands/resendNotification.php
+++ app/Console/Commands/resendNotification.php
@@ -40,54 +40,68 @@ class resendNotification extends Command {
 	public function fire()
 	{
 		$id = $this->argument('id');
-		$getPayment = Payment::where('address', $id)->orWhere('id', $id)->first();
-		if(!$getPayment){
-			$this->error('Payment not found');
-			return false;
-		}
-		$getSlot = Slot::find($getPayment->slotId);
-		if(trim($getSlot->webhook) == ''){
-			$this->error('No webhook for slot '.$getSlot->id);
-			return false;
-		}
-		
-		$complete = boolval($getPayment->complete);
-		$complete_txt = '';
-		if(boolval($this->argument('complete'))){
-			$complete = true;
-			$getPayment->complete = 1;
-			$getPayment->save();
-			$complete_txt = ' (completed)';
-		}
-		
-		$caller = app('Tokenly\XcallerClient\Client');
-		$hookData = array();
-		$hookData['payment_id'] = $getPayment->id;
-		$hookData['slot_id'] = $getSlot->public_id;
-		$hookData['reference'] = $getPayment->reference;
-		$hookData['payment_address'] = $getPayment->address;
-		$hookData['asset'] = $getPayment->token;
-		$hookData['total'] = Currency::satoshisToValue($getPayment->total);
-		$hookData['total_satoshis'] = $getPayment->total;
-		$hookData['received'] = Currency::satoshisToValue($getPayment->received);
-		$hookData['received_satoshis'] = $getPayment->received;
-		$hookData['confirmations'] = $getSlot->min_conf; 				
-		$hookData['init_date'] = $getPayment->init_date;
-		$hookData['complete'] = $complete;
-		$hookData['complete_date'] = $getPayment->complete_date;
-		$hookData['tx_info'] = json_decode($getPayment->tx_info, true);
-		
-		try{
-			$sendWebhook = $caller->sendWebhook($hookData, $getSlot->webhook);
-		}
-		catch(\Exception $e){
-			$this->error('Error sending notification '.$e->getMessage());
-			return false;
-		}
-		
-		
-		$this->info('Payment notification sent'.$complete_txt);
-		
+        $since = $this->option('since');
+        $caller = app('Tokenly\XcallerClient\Client');
+        if($since AND trim($since) != ''){
+            $date = date('Y-m-d H:i:s', strtotime($since));
+            $payment_list = Payment::where('created_at', '>=', $date)->get();
+            if(!$payment_list){
+                $payment_list = array();
+            }
+        }
+        else{
+            $getPayment = Payment::where('address', $id)->orWhere('id', $id)->first();
+            if(!$getPayment){
+                $this->error('Payment not found');
+                return false;
+            }            
+            $payment_list = array($getPayment);
+        }
+        
+        foreach($payment_list as $getPayment){
+            $getSlot = Slot::find($getPayment->slotId);
+            if(trim($getSlot->webhook) == ''){
+                $this->error('No webhook for slot '.$getSlot->id);
+                return false;
+            }
+            
+            $complete = boolval($getPayment->complete);
+            $complete_txt = '';
+            if(boolval($this->argument('complete'))){
+                $complete = true;
+                $getPayment->complete = 1;
+                $getPayment->save();
+                $complete_txt = ' (completed)';
+            }
+            
+            
+            $hookData = array();
+            $hookData['payment_id'] = $getPayment->id;
+            $hookData['slot_id'] = $getSlot->public_id;
+            $hookData['reference'] = $getPayment->reference;
+            $hookData['payment_address'] = $getPayment->address;
+            $hookData['asset'] = $getPayment->token;
+            $hookData['total'] = Currency::satoshisToValue($getPayment->total);
+            $hookData['total_satoshis'] = $getPayment->total;
+            $hookData['received'] = Currency::satoshisToValue($getPayment->received);
+            $hookData['received_satoshis'] = $getPayment->received;
+            $hookData['confirmations'] = $getSlot->min_conf; 				
+            $hookData['init_date'] = $getPayment->init_date;
+            $hookData['complete'] = $complete;
+            $hookData['complete_date'] = $getPayment->complete_date;
+            $hookData['tx_info'] = json_decode($getPayment->tx_info, true);
+            
+            try{
+                $sendWebhook = $caller->sendWebhook($hookData, $getSlot->webhook);
+            }
+            catch(\Exception $e){
+                $this->error('Error sending notification '.$e->getMessage());
+                return false;
+            }
+            
+            
+            $this->info('Payment notification sent'.$complete_txt);
+        }
 	}
 
 	/**
@@ -111,7 +125,7 @@ class resendNotification extends Command {
 	protected function getOptions()
 	{
 		return [
-			
+			array('since', null, InputOption::VALUE_REQUIRED, 'Ignore id and resend notifications for everything since X date')
 		];
 	}
 
